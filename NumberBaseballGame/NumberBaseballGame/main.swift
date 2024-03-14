1d9317c6810fa8e9bef2e22047f778fe7b8273ae
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-03-14 14:48:14 +0900

    Lv5 - 게임 기록 보기 완성

--- NumberBaseballGame/NumberBaseballGame/main.swift
+++ NumberBaseballGame/NumberBaseballGame/main.swift
@@ -16,6 +16,7 @@ func mainMenu() {
     print("환영합니다! 원하시는 번호를 입력해주세요.")
     print("1. 게임 시작하기  2. 게임 기록 보기  3. 종료하기")
     
+    
     if let menu = readLine() {
         if menu == "1" {            // 게임 시작하기
             if let guessCount = gameStart() {
@@ -84,17 +85,17 @@ func gameStart () -> Int? {
                     }
                     // 비교 결과 출력 & 게임 종료
                     if strike == 3 {
-                        print("정답입니다!")
+                        print("정답입니다!\n")
                         return guessCount
                     }
                     if strike > 0 && ball > 0 {
-                        print("\(strike)스트라이크 \(ball)볼")
+                        print("\(strike)스트라이크 \(ball)볼\n")
                     } else if strike > 0 {
-                        print("\(strike)스트라이크")
+                        print("\(strike)스트라이크\n")
                     } else if ball > 0 {
-                        print("\(ball)볼")
+                        print("\(ball)볼\n")
                     } else {
-                        print("Nothing")
+                        print("Nothing\n")
                     }
                 }
             }
